d82391a3dbf10466b31b462d78f2334c9fca3c22
Your Name <you@example.com>
2025-03-12 10:05:33 +0600

    category video complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const catagoryButton = document.getElementById("catagory-btn");
 const allVedioCard = document.getElementById("vedio");
-
+const detailsCard = document.getElementById("details-card");
+const modalSection = document.getElementById("modal-section");
 const recivedCatagoryBtn = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
         .then(res => res.json())
@@ -11,7 +12,7 @@ const recivedCatagoryBtn = () => {
 const displayCatagoryBtn = (catagories) => {
     catagories.forEach(cat => {
         const createBtn = document.createElement("div");
-        createBtn.innerHTML = `<button id="${cat.category_id}" class="bg-gray-500 text-white font-bold py-2 px-4 rounded-lg">${cat.category}</button>`;
+        createBtn.innerHTML = `<button onclick="recivedVedioWithCatagories(${cat.category_id})" id="${cat.category_id}" class="bg-gray-500 text-white font-bold py-2 px-4 rounded-lg">${cat.category}</button>`;
         catagoryButton.appendChild(createBtn);
     });
 
@@ -34,17 +35,21 @@ recivedCatagoryBtn();
 const recivedVedio = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then(res => res.json())
-        .then(listOfVedio => displayVedioAll(listOfVedio.videos))
+        .then(listOfVedio => {
+            displayVedioAll(listOfVedio.videos);
+        })
         .catch(err => console.log(err, "maara kha"));
 };
-
 recivedVedio();
 
+
 const displayVedioAll = (allVedios) => {
+    allVedioCard.innerHTML=""
+    
     allVedios.forEach(vedio => {
         const createCard = document.createElement("div");
         createCard.innerHTML = `
-        <div class="shadow-sm p-3 border border-bordercolor rounded-lg">
+        <div class="p-3 border border-bordercolor rounded-lg">
             <!-- image  -->
             <div class="h-44"><img src=${vedio.thumbnail} class=" h-full rounded-lg object-cover w-full" alt=""></div>
             <!-- text content  -->
@@ -59,15 +64,29 @@ const displayVedioAll = (allVedios) => {
                             <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
                             </svg>` : ""}
                         </p>
-                        <p class="text-gray-400 my-2 font-medium">${vedio.others.views} Views <button class="bg-primary font-bold text-lg px-2 py-1 rounded-lg text-white ml-5">Watch</button></p>
+                        <p class="text-gray-400 my-2 font-medium">${vedio.others.views} Views <button class="bg-primary font-bold text-lg px-2 py-1 rounded-lg text-white ml-5" onclick="details(${vedio.video_id})">Watch</button></p>
                     </div>
                 </div>
             </div>
         </div>
     `;
-    console.log(createCard);
-    allVedioCard.append(createCard)
-
-
+        allVedioCard.append(createCard)
     });
+};
+function details(Id) {
+    console.log(Id);
+}
+const closeBtn = document.getElementById("close-btn")
+closeBtn.addEventListener("click", () => {
+    detailsCard.classList.add("hidden");
+})
+// load vedio by catagories
+const recivedVedioWithCatagories = (catId) => {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${catId}`)
+        .then(res => res.json())
+        .then(listOfVedio => {
+            displayVedioAll(listOfVedio.
+                category);
+        })
+        .catch(err => console.log(err, "maara kha"));
 };
